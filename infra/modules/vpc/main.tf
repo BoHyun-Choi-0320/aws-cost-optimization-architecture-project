e48738548e1d732dd78482b9caf1f09a6dad1bc9
BoHyun-Choi-0320 <320fleur@gmail.com>
2025-11-20 01:34:25 +0900

    EDIT  Terraform Cloud : Basic Architecture - VPC

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -33,12 +33,6 @@ resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
-
-  tags = {
-    Name = "${var.name_prefix}-private-subnet-${count.index+1}"
-    "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
-    "kubernetes.io/role/internal-elb"               = "1"
-  }
 }
 
 # Database Subnets
