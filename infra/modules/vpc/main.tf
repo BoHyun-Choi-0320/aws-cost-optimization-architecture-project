c1e1d031a2cb9b49c1a3373da37e80e1fe36c8b5
BoHyun-Choi-0320 <320fleur@gmail.com>
2025-11-20 01:54:42 +0900

    EDIT  Terraform Cloud 3: Basic - Edit Subnet Tag

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -19,6 +19,10 @@ resource "aws_subnet" "public" {
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name_prefix}-pub-subnet-${count.index+1}"
+  }
 }
 
 # Private Subnets
@@ -27,6 +31,10 @@ resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.name_prefix}-pri-subnet-${count.index+1}"
+  }
 }
 
 # Database Subnets
